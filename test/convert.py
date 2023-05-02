e9d67f86eb368f04c6aed1977f86e84d787b29f0
0xsinex <93716089+0xsinex@users.noreply.github.com>
2023-05-02 11:14:12 +0300

    Cleaned convert.py

--- test/convert.py
+++ test/convert.py
@@ -1,21 +1,19 @@
 import pgntofen
 
-#filenames = ["pgndata/data00.pgn","pgndata/data01.pgn", "pgndata/data02.pgn", "pgndata/data03.pgn", "pgndata/data04.pgn", "pgndata/data05.pgn", "pgndata/data06.pgn","pgndata/data07.pgn", "pgndata/data08.pgn", "pgndata/data09.pgn"]
-# for testdata
-filenames = ["pgndata/data06.pgn"]#, "pgndata/data05.pgn"] #, "pgndata/data02.pgn", "pgndata/data03.pgn"]
-#filenames = ["fischer_keres.pgn"]
+filenames = ["path/to/file1", "path/to/file2"]
+
 pgnConverter = pgntofen.PgnToFen()
 for filename in filenames:
     pgnConverter.resetBoard()
     stats = pgnConverter.pgnFile(filename)
     
     # Output file for FEN
-    f = open("strides6_fen", "a")
+    f = open("path/to/output/file", "a")
     print("#### succeeded:", len(stats['succeeded']), "failed:", len(stats['failed']), "#####")
     for game in stats['succeeded']:
         try:
             fenlist = game[-1]
-            # write in batches of 12 with a stride of 3
+            # writes in batches of 12 with a stride of 3, replace with regular writing if needed
             stride = 9
             chunk_size = 12
             chunks = [fenlist[i:i+chunk_size] for i in range(0, len(fenlist)-chunk_size+1, stride)]
